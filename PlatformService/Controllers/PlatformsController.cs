4da518841daa12a5c0f66302073902ca968290d7
Geany <catalinalbert@yahoo.com>
2023-02-22 18:29:51 +0200

    Add project files.

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/platforms")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult <IEnumerable<PlatformReadDTO>> GetPlatforms()
+        {
+            Console.WriteLine("-----Getting Platform-----");
+
+            var platformItem = _platformRepo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDTO>>(platformItem));
+        }
+        [HttpGet("{id}", Name = "GetPlatformById")]
+
+        public ActionResult<PlatformReadDTO> GetPlatformById(int id)
+        {
+            Console.WriteLine("-----Getting One Platform-----");
+
+            var platformItem = _platformRepo.GetPlatformById(id);
+
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDTO>(platformItem));
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult <PlatformReadDTO> CreatePlatform(PlatformCreateDTO platformCreateDTO)
+        {
+            Console.WriteLine("-----Creating Platform-----");
+
+            var platformModel = _mapper.Map<Platform>(platformCreateDTO);
+            _platformRepo.CreatePlatform(platformModel);
+            _platformRepo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDTO>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformReadDto.Id}, platformReadDto);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,35 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("No DATA");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "DOT NET", Publisher = "Microsoft", Price = 0 },
+                    new Platform() { Name = "SQL SERVER EXPRESS", Publisher = "Microsoft", Price = 50 },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Foundation", Price = 100 }
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("THERE IS DATA");
+            }
+        }
+    }
+}
